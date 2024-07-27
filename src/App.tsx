46cea94acd1f3909a37efec76ea16ae493f43e02
okky23 <okkypratama.dev@gmail.com>
2024-07-27 23:02:29 +0700

    add moviecard detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,93 @@
-import './App.css'
-import { Card } from './components/Card'
-import { Navbar } from './components/Navbar'
+import "./App.css";
+import { Card } from "./components/Card";
+import { Navbar } from "./components/Navbar";
+
+const movies = [
+  {
+    title: "The Avengers",
+    releaseDate: "2012-04-25",
+    rating: 8.3,
+    imageUrl: "https://images5.alphacoders.com/573/573475.jpg",
+    genre: "Adventure, Science Fiction, Action",
+    tagline: "Part of the journey is the end.",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+  },
+  {
+    title: "The Avengers",
+    releaseDate: "2012-04-25",
+    rating: 8.3,
+    imageUrl: "https://images5.alphacoders.com/573/573475.jpg",
+    genre: "Adventure, Science Fiction, Action",
+    tagline: "Part of the journey is the end.",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+  },
+  {
+    title: "The Avengers",
+    releaseDate: "2012-04-25",
+    rating: 8.3,
+    imageUrl: "https://images5.alphacoders.com/573/573475.jpg",
+    genre: "Adventure, Science Fiction, Action",
+    tagline: "Part of the journey is the end.",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+  },
+  {
+    title: "The Avengers",
+    releaseDate: "2012-04-25",
+    rating: 8.3,
+    imageUrl: "https://images5.alphacoders.com/573/573475.jpg",
+    genre: "Adventure, Science Fiction, Action",
+    tagline: "Part of the journey is the end.",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+  },
+  {
+    title: "The Avengers",
+    releaseDate: "2012-04-25",
+    rating: 8.3,
+    imageUrl: "https://images5.alphacoders.com/573/573475.jpg",
+    genre: "Adventure, Science Fiction, Action",
+    tagline: "Part of the journey is the end.",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+  },
+  {
+    title: "The Avengers",
+    releaseDate: "2012-04-25",
+    rating: 8.3,
+    imageUrl: "https://images5.alphacoders.com/573/573475.jpg",
+    genre: "Adventure, Science Fiction, Action",
+    tagline: "Part of the journey is the end.",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+  },
+];
 
 function App() {
-
   return (
     <>
-     <Navbar />
-     <h3 className='font-bold ml-10 mt-7 text-lg'>Home</h3>
-     <div className="p-4  min-h-screen">
-      <Card
-        title="The Avengers"
-        releaseDate="2012-04-25"
-        rating={7.7}
-        imageUrl="https://images5.alphacoders.com/573/573475.jpg"
-      />
-    </div>
+      <Navbar />
+      <h3 className="font-bold ml-10 mt-7 text-lg">Home</h3>
+      <div className="p-4  min-h-screen">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+          {movies.map((movie, index) => (
+            <Card
+              key={index}
+              title={movie.title}
+              releaseDate={movie.releaseDate}
+              rating={movie.rating}
+              imageUrl={movie.imageUrl}
+              genre={movie.genre}
+              tagline={movie.tagline}
+              description={movie.description}
+            />
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,30 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { DetailCard } from "./DetailCard";
 
 interface CardProps {
-  title: string;
-  releaseDate: string;
-  rating: number;
-  imageUrl: string;
-}
+    title: string;
+    releaseDate: string;
+    rating: number;
+    imageUrl: string;
+    genre: string;
+    tagline: string;
+    description: string;
+  }
 export const Card: React.FC<CardProps> = ({
-  title,
-  releaseDate,
-  rating,
-  imageUrl,
+    title,
+    releaseDate,
+    rating,
+    imageUrl,
+    genre,
+    tagline,
+    description,
 }) => {
-  return (
-    <div className="relative w-64 rounded-xl overflow-hidden shadow-lg">
-      <img src={imageUrl} alt={title} className="w-full h-96 object-cover" />
+   const [isModalOpen, setIsModalOpen] = useState(false)
 
-      <div className="absolute bottom-0 left-0 right-0 bg-white p-3">
-        <div className="absolute -top-5 left-3 bg-blue-950 border-2 border-green-500 text-white text-sm font-bold w-10 h-10 rounded-full flex items-center justify-center">
-          {rating.toFixed(1)}
-        </div>
-        <div className="text-center">
-          <h2 className="text-black text-lg font-bold ">{title}</h2>
-          <p className="text-gray-600 text-sm">{releaseDate}</p>
+  return (
+    <>
+   <div
+        className="relative w-64 rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <img src={imageUrl} alt={title} className="w-full h-96 object-cover" />
+
+        <div className="absolute bottom-0 left-0 right-0 bg-white p-3">
+          <div className="absolute -top-5 left-3 bg-blue-950 border-2 border-green-500 text-white text-sm font-bold w-10 h-10 rounded-full flex items-center justify-center z-10">
+            {rating.toFixed(1)}
+          </div>
+          <div className="text-center">
+            <h2 className="text-black text-lg font-bold truncate">{title}</h2>
+            <p className="text-gray-600 text-sm">{releaseDate}</p>
+          </div>
         </div>
       </div>
-    </div>
+
+      {isModalOpen && (
+        <DetailCard
+          title={title}
+          imageUrl={imageUrl}
+          rating={rating}
+          genre={genre}
+          tagline={tagline}
+          description={description}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+    
+    </>
   );
 };
